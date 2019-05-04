1ada2c07ae72ce07c0cbe27879e327f990439e1f
lsy365 <35445594+lsy365@users.noreply.github.com>
2019-05-05 07:44:07 +0800

    Update game_manager.h

--- Boost_flyChess_server-master/flyChess_server-master/src/game_manager.h
+++ Boost_flyChess_server-master/flyChess_server-master/src/game_manager.h
@@ -13,6 +13,7 @@ class GameManager : public std::enable_shared_from_this<GameManager>
 {
 public:
     GameManager();
+    // 这个智能指针不要改, 就是不要改make_shared, 我改了就错也不知道为什么
     void addPlayer(TcpConnection::Pointer);             //将shared_ptr保存在map中
     void removePlayer(TcpConnection::Pointer);
     void processClientMsg(TcpConnection::Pointer, const std::string& msg); //分发，处理客户端发来的信息
@@ -21,9 +22,10 @@ private:
     void removePlayerFromHisRoom(Player::Pointer);
     void selectRoom(Player::Pointer , const std::string &msg);
     bool changeRoom(Player::Pointer , int roomNum);
-
-    void login(Player::Pointer , const std::string &msg);
+    
     void ping(Player::Pointer);
+    void login(Player::Pointer , const std::string &msg);
+    // 到时候register函数可以选择删掉, 那么在TCP_conn和server里面都要对应删掉
     void registerAccount(Player::Pointer,  const std::string &msg);
 
 private:
@@ -43,6 +45,5 @@ private:
 //game quitroom 从房间退出
 //game rob 1抢地主
 //game norob 不抢
-//game play 0 1 2 3 4 5 出牌
-//game noplay
+//game play 1,2,3,4,5,6 出牌
 //ping 心跳
